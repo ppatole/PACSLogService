f63ae7fc6fcc831669c91711dbd1dc4a6390aee3
ppatole <ppatole@twinpod.com>
2016-09-15 19:17:43 +0530

    First code Checkin.. recording log in file

--- PACSLogService/RecordLog.svc.cs
+++ PACSLogService/RecordLog.svc.cs
@@ -10,24 +10,13 @@ namespace PACSLogService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
-    {
-        public string GetData(int value)
+    public class Service1 : IRecordLog
+    {        
+        [WebGet]
+        public void LogThis(string XMLValue)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+            Common.Log l = new Common.Log("PacsLog",true);
+            l.WriteFilelog("PACSLogService","", XMLValue);
+        }       
     }
 }
